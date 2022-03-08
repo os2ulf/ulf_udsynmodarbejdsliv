6a0dd7f4ac685c134dbacf95898faf293794869c
rfo <rfo@novicell.dk>
2022-03-08 12:41:17 +0100

    OS2ULF-523: Change colors and button link

--- themes/ulf_hjoerring/templates/node/node--internship.tpl.php
+++ themes/ulf_hjoerring/templates/node/node--internship.tpl.php
@@ -163,7 +163,7 @@
                       <?php print render($content['field_last_signup_date']); ?>
 
                       <?php if ($field_signup_show_link[LANGUAGE_NONE][0]['value'] === '1'): ?>
-                          <?php print l(t('How to apply for an internship'), '/sådan-søger-du-erhvervspraktik', ['attributes' => ['class' => 'block--signup-link']]) ?>
+                          <?php print l(t('How to apply for an internship'), '/tilmelding', ['attributes' => ['class' => 'block--signup-link']]) ?>
                       <?php endif; ?>
                     </div>
                   </div>
